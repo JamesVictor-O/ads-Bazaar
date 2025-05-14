7005e0e3a3b4ca2cabaa13f3c525d9821d67b810
jerrymusaga <musagatechnology@gmail.com>
2025-05-14 04:38:43 +0100

    Comprehensive Smart Contract Testing for AdsBazaar

--- contract/script/AdsBazaar.s.sol
+++ contract/script/AdsBazaar.s.sol
@@ -5,15 +5,23 @@ import {Script, console} from "forge-std/Script.sol";
 import {AdsBazaar} from "../src/AdsBazaar.sol";
 
 contract DeployAdsBazaar is Script {
+   
+    address constant CELO_ALFAJORES_cUSD = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+        
+        console.log("Deployer address:", deployer);
+        console.log("Using Alfajores cUSD at:", CELO_ALFAJORES_cUSD);
+
         vm.startBroadcast(deployerPrivateKey);
         
-        address cUSD = 0x874069fa1eb16d44d622f2e0ca25eea172369bc1; 
-        AdsBazaar adsBazaar = new AdsBazaar(cUSD);
+        AdsBazaar adsBazaar = new AdsBazaar(CELO_ALFAJORES_cUSD);
         
         vm.stopBroadcast();
-        
+
         console.log("AdsBazaar deployed at:", address(adsBazaar));
+        
     }
 }
