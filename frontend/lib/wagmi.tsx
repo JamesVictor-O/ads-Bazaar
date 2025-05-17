fb069b3e5090d2a7eb42fdc0dfde7d15352296bc
JamesVictor-o <victorjames40v@gmail.com>
2025-05-17 18:25:07 +0100

    worked on the wallet pop up

--- frontend/lib/wagmi.tsx
+++ frontend/lib/wagmi.tsx
@@ -1,9 +1,34 @@
 
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, sepolia,celoAlfajores} from 'wagmi/chains';
+import { getDefaultConfig, connectorsForWallets } from '@rainbow-me/rainbowkit';
+import { celoAlfajores } from 'wagmi/chains';
+import { injectedWallet, metaMaskWallet } from '@rainbow-me/rainbowkit/wallets';
+import { http } from 'wagmi';
+
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [
+        injectedWallet, // Browser-injected wallets (e.g., MetaMask)
+        metaMaskWallet, // MetaMask specifically
+      
+      ],
+    },
+  ],
+  {
+    appName: 'Ads-bazzar',
+    projectId: "YOUR_PROJECT_ID",
+  }
+);
 
 export const wagmiConfig = getDefaultConfig({
-  appName: 'Ads-bazzar', 
-  projectId: "YOUR_PROJECT_ID", 
-  chains: [mainnet, celoAlfajores], 
+  appName: 'Ads-bazzar',
+  projectId: "YOUR_PROJECT_ID",
+  chains: [celoAlfajores],
+  connectors, // Use the custom connectors list
+  reconnect: false, // Still disable reconnect for safety,
+  transports: {
+    [celoAlfajores.id]: http("https://alfajores-forno.celo-testnet.org"),
+  }
 });
