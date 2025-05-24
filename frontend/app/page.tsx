d65b87c6cdbcdb7c2d4de4e8977968c9588b2bba
jerrymusaga <musagatechnology@gmail.com>
2025-05-24 20:11:09 +0100

    setting up metatags

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import HeroSection from "@/components/landingPage/hero-section";
 import GetStartedModal from "@/components/modals/GetStartedModal";
-import Head from "next/head";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -13,26 +12,6 @@ export default function Home() {
 
   return (
     <div>
-      <Head>
-        <meta
-          name="fc:frame"
-          content='{
-      "version": "next",
-      "imageUrl": "https://ads-bazaar.vercel.app/adsBazaar-heroPage.png",
-      "button":{
-        "title": "AdsBazaar",
-        "action": {
-          "type": "launch_frame",
-          "name": "Ads-Bazaar",
-          "url": "https://ads-bazaar.vercel.app/",
-          "splashImageUrl": "https://ads-bazaar.vercel.app/adsBazaar-logo.png",
-          "splashBackgroundColor": "#059669"
-        }
-      }
-    }'
-          data-rh="true"
-        ></meta>
-      </Head>
       <HeroSection setIsModalOpen={setIsModalOpen} />
 
       {/* Modal */}
