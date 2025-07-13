0dc31e1c46a351859e0d656749f878a26f2b49b5
JamesVictor-o <victorjames40v@gmail.com>
2025-07-13 15:59:34 +0100

    fic

--- frontend/app/influencersDashboard/page.tsx
+++ frontend/app/influencersDashboard/page.tsx
@@ -706,10 +706,10 @@ export default function InfluencerDashboard() {
             {!isVerified && (
               <Link href="/selfVerification">
                 <motion.button
-                  className="flex items-center gap-1.5 md:gap-2 px-3 py-1.5 md:px-4 md:py-2 bg-gradient-to-r from-amber-500 to-orange-500 my-2 text-white rounded-lg md:rounded-xl font-medium transition-all text-sm md:text-base mt-3"
+                  className="flex items-center gap-1.5 md:gap-2 px-3 py-1.5 md:px-4 md:py-2 bg-gradient-to-r from-amber-500 to-orange-500  text-white rounded-lg md:rounded-xl font-medium transition-all text-sm md:text-base md:mt-4"
                   whileTap={{ scale: 0.95 }}
                 >
-                  <span className="">Get Verified Now</span>
+                  <span className="text-xl">Get Verified Now</span>
                   <ArrowRight className="w-3 h-3 md:w-4 md:h-4" />
                 </motion.button>
               </Link>
