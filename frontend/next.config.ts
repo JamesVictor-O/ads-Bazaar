2bdd932ec7218ba89d6e465669ab338828531468
jerrymusaga <musagatechnology@gmail.com>
2025-07-16 07:46:46 +0100

    modify config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['imagedelivery.net','lh3.googleusercontent.com','i.seadn.io'],
   },
